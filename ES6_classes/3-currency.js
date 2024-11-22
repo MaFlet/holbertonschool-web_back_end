69162b311460344202fb9379730216df5b552dbe
MaFlet <mfletcher2808@hotmail.com>
2024-11-22 17:02:56 +1100

    Task 3

--- ES6_classes/3-currency.js
+++ ES6_classes/3-currency.js
@@ -1,33 +1,32 @@
 export default class Currency {
-    constructor(code, name) {
-      this._code = code;
-      this._name = name;
-    }
+  constructor(code, name) {
+    this._code = code;
+    this._name = name;
+  }
 
-    get code() {
-        return this._code;
-      }
-    
-      set code(newCode) {
-        if (typeof newCode !== 'string') {
-          throw new TypeError('New code must be a string');
-        }
-        this._code = newCode;
+  get code() {
+    return this._code;
+  }
+
+  set code(newCode) {
+    if (typeof newCode !== 'string') {
+      throw new TypeError('New code must be a string');
     }
+    this._code = newCode;
+  }
+
+  get name() {
+    return this._name;
+  }
+
+  set name(newName) {
+    if (typeof newName !== 'string') {
+      throw new TypeError('Name must be a string');
+    }
+    this._name = newName;
+  }
   
-    get name() {
-      return this._name;
-    }
-  
-    set name(newName) {
-      if (typeof newName !== 'string') {
-        throw new TypeError('Name must be a string');
-      }
-      this._name = newName;
-    }
-    
-    displayFullCurrency() {
-        return `${this._name} (${this._code})`;
-    }
+  displayFullCurrency() {
+    return `${this._name} (${this._code})`;
+  }
 }
-  
