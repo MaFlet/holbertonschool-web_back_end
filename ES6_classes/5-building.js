047b4abe28b38423ec0f242311a30805f2e687e2
MaFlet <mfletcher2808@hotmail.com>
2024-11-22 19:44:00 +1100

    Task 5

--- ES6_classes/5-building.js
+++ ES6_classes/5-building.js
@@ -1,13 +1,13 @@
 export default class Building {
   constructor(sqft) {
-    if (this.constructor !== Building &&
-        !this.evacuationWarningMessage) {
-            throw new Error('Class extending Building must override evacuationWarningMessage')
+    if (this.constructor !== Building
+            && !this.evacuationWarningMessage) {
+      throw new Error('Class extending Building must override evacuationWarningMessage');
     }
     this._sqft = sqft;
   }
 
   get sqft() {
     return this._sqft;
-}
+  }
 }
