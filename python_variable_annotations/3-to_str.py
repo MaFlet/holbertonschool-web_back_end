b07e1bd252563e0313b60d596dfcf81fc81fb183
MaFlet <mfletcher2808@hotmail.com>
2024-12-04 21:01:20 +1100

    Task 3

---
+++ python_variable_annotations/3-to_str.py
@@ -0,0 +1,13 @@
+#!/usr/bin/env python3
+"""
+Module for basic arithmetic operations
+using type-annotated functions.
+"""
+
+
+def to_str(n: float) -> str:
+    """
+    Return the string representation of
+    float.
+    """
+    return str(n)
