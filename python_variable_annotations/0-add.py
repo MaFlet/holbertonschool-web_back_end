adb10823a2b0fd1e43275e70b5f46474991828fe
MaFlet <mfletcher2808@hotmail.com>
2024-12-04 20:36:07 +1100

    Task 0

--- python_variable_annotations/0-add.py
+++ python_variable_annotations/0-add.py
@@ -1,3 +1,11 @@
 #!/usr/bin/env python3
+"""
+Module for basic arithmetic operations
+using type-annotated functions.
+"""
 def add(a: float, b: float) -> float:
+    """
+    Return the sum of two floating-point
+    numbers.
+    """
     return a + b
