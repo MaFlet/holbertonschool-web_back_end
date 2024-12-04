8f5c5688ed2bf2ec82fec1e5b91fe908990cd367
MaFlet <mfletcher2808@hotmail.com>
2024-12-04 21:37:49 +1100

    Task 5

---
+++ python_variable_annotations/5-sum_list.py
@@ -0,0 +1,12 @@
+#!/usr/bin/env python3
+"""
+Module for basic listing operations
+using type-annotated functions.
+"""
+
+
+def sum_list(input_list: float) -> float:
+    """
+    Return sum as a float.
+    """
+    return sum(input_list)
