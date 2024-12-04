634e78dbce322d9d0769eac0edb4941b38b7cf82
MaFlet <mfletcher2808@hotmail.com>
2024-12-04 20:44:56 +1100

    Task 1

---
+++ python_variable_annotations/1-concat.py
@@ -0,0 +1,12 @@
+#!/usr/bin/env python3
+"""
+Module for basic string operations
+using type-annotated functions.
+"""
+
+
+def concat(str1: str, str2: str) -> str:
+    """
+    Return the concatenated string.
+    """
+    return str1 + str2
