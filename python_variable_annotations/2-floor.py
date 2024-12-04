ba7fbfbaa86be921808d953cfd891a27fcfea525
MaFlet <mfletcher2808@hotmail.com>
2024-12-04 20:52:10 +1100

    Task 2

---
+++ python_variable_annotations/2-floor.py
@@ -0,0 +1,12 @@
+#!/usr/bin/env python3
+"""
+Module for basic arithmetic operations
+using type-annotated functions.
+"""
+
+
+def floor(n: float) -> float:
+    """
+    Return floor of float.
+    """
+    return n
