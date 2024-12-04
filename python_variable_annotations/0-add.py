35ac2e07c92d96cfd7560a34704b8f7a9d047ee5
MaFlet <mfletcher2808@hotmail.com>
2024-12-04 20:33:45 +1100

    Task 0

--- python_variable_annotations/0-add.py
+++ python_variable_annotations/0-add.py
@@ -1,3 +1,8 @@
 #!/usr/bin/env python3
+"""
+Script that write type-annotated function add that
+takes float a and b as arguments and return sum as 
+float
+"""
 def add(a: float, b: float) -> float:
     return a + b
