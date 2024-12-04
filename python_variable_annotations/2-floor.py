3681e47e02e934772ccfa262342388ae3f33036c
MaFlet <mfletcher2808@hotmail.com>
2024-12-04 20:54:02 +1100

    Task 2

--- python_variable_annotations/2-floor.py
+++ python_variable_annotations/2-floor.py
@@ -3,10 +3,11 @@
 Module for basic arithmetic operations
 using type-annotated functions.
 """
+import math
 
 
-def floor(n: float) -> float:
+def floor(n: float) -> int:
     """
     Return floor of float.
     """
-    return n
+    return math.floor(n)
