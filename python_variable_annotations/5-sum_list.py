a8b32275e8dd784a238ae8bd0575b94e73cc3683
MaFlet <mfletcher2808@hotmail.com>
2024-12-04 21:39:09 +1100

    Task 5

--- python_variable_annotations/5-sum_list.py
+++ python_variable_annotations/5-sum_list.py
@@ -3,9 +3,10 @@
 Module for basic listing operations
 using type-annotated functions.
 """
+from typing import List
 
 
-def sum_list(input_list: float) -> float:
+def sum_list(input_list: List[float]) -> float:
     """
     Return sum as a float.
     """
