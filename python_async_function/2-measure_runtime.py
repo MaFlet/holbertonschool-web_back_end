be615540b182852efa093a5c4d8ee8c06320b142
MaFlet <mfletcher2808@hotmail.com>
2024-12-21 17:47:52 +1100

    Task 2

---
+++ python_async_function/2-measure_runtime.py
@@ -0,0 +1,17 @@
+#!/usr/bin/env python3
+"""Asynchronous python programming"""
+import asyncio
+import time
+wait_n = __import__('1-concurrent_coroutines').wait_n
+
+
+def measure_time(n: int, max_delay: int) -> float:
+    """
+    Measures the total execution time. Will
+    return float
+    """
+    start_time = time.perf_counter()
+    asyncio.run(wait_n(n, max_delay))
+    total_time = time.perf_counter() - start_time
+
+    return total_time / n
