7107aa064cdc8ca6e30e24411c1c9835e40150bc
MaFlet <mfletcher2808@hotmail.com>
2024-12-21 15:37:08 +1100

    Task 9

--- python_variable_annotations/9-element_length.py
+++ python_variable_annotations/9-element_length.py
@@ -5,6 +5,7 @@ using type-annotated functions.
 """
 from typing import List, Tuple, Iterable, Sequence
 
+
 def element_length(lst: Iterable[Sequence]) -> List[Tuple[Sequence, int]]:
     """Return values with appropriate types"""
     return [(i, len(i)) for i in lst]
