1aa908adf5f72ab45b6de9082593687736d7532b
MaFlet <mfletcher2808@hotmail.com>
2024-12-23 09:40:57 +1100

    Task 2

---
+++ python_async_comprehension/2-measure_runtime.py
@@ -0,0 +1,22 @@
+#!/usr/bin/env python3
+"""Module script for async generators"""
+import asyncio
+import time
+async_comprehension = __import__('1-async_comprehension').async_comprehension
+
+
+async def measure_runtime() -> float:
+    """
+    Coroutine that wil execute async_comprehension
+    4 times in parallel using asyncio.gather. It
+    should measure total runtime and return it.
+    """
+    start_time = time.perf_counter()
+    await asyncio.gather(
+        async_comprehension(),
+        async_comprehension(),
+        async_comprehension(),
+        async_comprehension()
+    )
+    end_time = time.perf_counter()
+    return end_time - start_time
