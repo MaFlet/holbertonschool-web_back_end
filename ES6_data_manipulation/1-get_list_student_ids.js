43ee161450cbb64e3e452298a4d40a8dfb2ecea3
MaFlet <mfletcher2808@hotmail.com>
2024-12-23 11:06:43 +1100

    Task 1

--- ES6_data_manipulation/1-get_list_student_ids.js
+++ ES6_data_manipulation/1-get_list_student_ids.js
@@ -1,23 +1,5 @@
-function getListStudentIds() {
-  const students = [
-    {
-      id: 1,
-      firstName: 'Guillaume',
-      location: 'San Francisco',
-    },
-    {
-      id: 2,
-      firstName: 'James',
-      location: 'Columbia',
-    },
-    {
-      id: 5,
-      firstName: 'Serena',
-      location: 'San Francisco',
-    },
-  ];
-
-  if (students.length === 0) {
+function getListStudentIds(students) {
+  if (!students || !Array.isArray(students) || students.length === 0) {
     return [];
   }
   return students.map((student) => student.id);
