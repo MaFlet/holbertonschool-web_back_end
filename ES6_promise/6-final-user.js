2056fd6fe3342de41f33c59dc86e71135b9fd920
MaFlet <mfletcher2808@hotmail.com>
2024-12-26 20:29:47 +1100

    Task 6

--- ES6_promise/6-final-user.js
+++ ES6_promise/6-final-user.js
@@ -5,11 +5,8 @@ export default function handleProfileSignup(firstName, lastName, filename) {
   const promises = [signUpUser(firstName, lastName), uploadPhoto(filename)];
 
   return Promise.allSettled(promises)
-    .then((results) =>
-      results.map((result) => ({
+    .then((results) => results.map((result) => ({
         status: result.status,
         value: result.status === 'fulfilled' ? result.value : String(result.reason),
-      })),
-
-    );
+      })));
 }
