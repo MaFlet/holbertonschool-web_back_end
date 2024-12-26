097ce7943468488f94c3b099093f0703c126ca25
MaFlet <mfletcher2808@hotmail.com>
2024-12-26 20:30:43 +1100

    Task 6

--- ES6_promise/6-final-user.js
+++ ES6_promise/6-final-user.js
@@ -6,7 +6,7 @@ export default function handleProfileSignup(firstName, lastName, filename) {
 
   return Promise.allSettled(promises)
     .then((results) => results.map((result) => ({
-        status: result.status,
-        value: result.status === 'fulfilled' ? result.value : String(result.reason),
-      })));
+      status: result.status,
+      value: result.status === 'fulfilled' ? result.value : String(result.reason),
+    })));
 }
