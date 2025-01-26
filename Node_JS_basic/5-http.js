c72afd50f0208bca3814ee0e57c2c4c00742489b
MaFlet <mfletcher2808@hotmail.com>
2025-01-26 12:07:41 +1100

    Task 5

--- Node_JS_basic/5-http.js
+++ Node_JS_basic/5-http.js
@@ -1,20 +1,20 @@
-// Import the http module from Node.js
 const http = require('http');
 const countStudents = require('./3-read_file_async');
 
-const app = http.createServer(async (req, res) => {
+const app = http.createServer((req, res) => {
   res.writeHead(200, { 'Content-Type': 'text/plain' });
 
   if (req.url === '/') {
     res.end('Hello Holberton School!');
   } else if (req.url === '/students') {
-    const output = 'This is the list of our students\n';
-    try {
-      const data = await countStudents(process.argv[2]);
-      res.end(output + data);
-    } catch (error) {
-      res.end(output + error.message);
-    }
+    const outputMessage = 'This is the list of our students\n';
+    countStudents(process.argv[2])
+      .then(() => {
+        res.end(outputMessage);
+      })
+      .catch((error) => {
+        res.end(outputMessage + error.message);
+      });
   }
 });
 
