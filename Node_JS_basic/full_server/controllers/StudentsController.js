efa5579bf83b7ec3820152f7815e6132d810f7ff
MaFlet <mfletcher2808@hotmail.com>
2025-01-26 20:48:21 +1100

    Task 8

---
+++ Node_JS_basic/full_server/controllers/StudentsController.js
@@ -0,0 +1,42 @@
+const readDatabase = require('../utils');
+
+class StudentsController {
+    static getAllStudents(request, response) {
+        readDatabase(process.argv[2])
+          .then((fields) => {
+            const output = ['This is the list of our students'];
+
+            const sortedFields = Object.keys(fields).sort((a, b) =>
+              a.localeCompare(b, undefined, { sensitivity: 'base' }));
+
+            sortedFields.forEach((field) => {
+              output.push(`Number of students in ${field}: ${fields[field].length}. List: ${fields[field].join(', ')}`);
+            });
+
+            response.status(200).send(output.join('\n'));
+          })
+          .catch(() => {
+            response.status(500).send('Cannot load the database');
+          });
+    }
+
+    static getAllStudentsByMajor(request, response) {
+      const { major } = request.params;
+
+      if (major !== 'CS' && major !== 'SWE') {
+        response.status(500).send('Major parameter must be CS or SWE');
+        return;
+      }
+
+      readDatabase(process.argv[2])
+        .then((fields) => {
+          const students = fields[major] || [];
+          response.status(200).send(`List: ${students.join(', ')}`);
+        })
+        .catch(() => {
+          response.status(500).send('Cannot load the database');
+        });
+    }
+}
+
+module.exports = StudentsController;
