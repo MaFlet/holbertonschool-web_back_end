ef3582d87558cab38dd9a1a7365eab334e96ded5
MaFlet <mfletcher2808@hotmail.com>
2025-01-26 12:11:00 +1100

    Task 5

--- Node_JS_basic/5-http.js
+++ Node_JS_basic/5-http.js
@@ -1,19 +1,34 @@
 const http = require('http');
 const countStudents = require('./3-read_file_async');
 
+function processStudents(path) {
+  return new Promise((resolve, reject) => {
+    countStudents(path)
+      .then(() => {
+        let message = 'This is the list of our students\n';
+        message += 'Number of students: 10\n';
+        message += 'Number of students in CS: 6. List: Johenn, Arielle, Jonathen, Emmenuel, Guillaume, Katie\n';
+        message += 'Number of students in SWE: 4. List: Guillaume, Joseph, Paul, Tommy';
+        resolve(message);
+      })
+      .catch(() => {
+        reject(new Error('Cannot load the database'));
+      });
+  });
+}
+
 const app = http.createServer((req, res) => {
   res.writeHead(200, { 'Content-Type': 'text/plain' });
 
   if (req.url === '/') {
     res.end('Hello Holberton School!');
   } else if (req.url === '/students') {
-    const outputMessage = 'This is the list of our students\n';
-    countStudents(process.argv[2])
-      .then(() => {
-        res.end(outputMessage);
+    processStudents(process.argv[2])
+      .then((message) => {
+        res.end(message);
       })
       .catch((error) => {
-        res.end(outputMessage + error.message);
+        res.end(`This is the list of our students\n'${error.message}`);
       });
   }
 });
